713822a7208efa47896a1623fd5a3f137f0eec12
unknown <amezcua.carrillo.jorge.alejandro@gmail.com>
2023-05-27 12:22:31 -0600

    allow cors origin

--- src/veterinaria/index.js
+++ src/veterinaria/index.js
@@ -2,12 +2,17 @@ const { Pool } = require("pg");
 // import Pool from 'pg'
 
 const express = require("express")
+const cors = require('cors');
 const app = express()
 
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(require("./src/routes/index"))
+
 
 app.listen(8000)
 console.log("siiuuu")
