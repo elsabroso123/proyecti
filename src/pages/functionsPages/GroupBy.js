16fb6ecf4b1ce875e2859958b84df9188a2f5422
unknown <amezcua.carrillo.jorge.alejandro@gmail.com>
2023-05-27 15:49:47 -0600

    adding functions db in frontend

--- src/pages/functionsPages/GroupBy.js
+++ src/pages/functionsPages/GroupBy.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const GroupBy = () => {
+
+    const [data, setData] = useState()
+    const [search, setSearch] = useState('')
+    const [razas, setRazas] = useState(null)
+
+    useEffect(() => {
+        fetch('http://localhost:8000/getData/Mascota/')
+        .then(response => response.json())
+        .then(data => setData(data))
+    }, [])
+
+    const Buscar = () => {
+        setRazas(data.filter(raza => search.toLowerCase()===raza.raza.toLowerCase()))
+        console.log(razas)
+    }
+
     return(
-        <div>Group By Raza</div>
+        <div>
+            <h3>Agrupar por razas de mascotas</h3>
+
+            <input type="text" placeholder="Buscar Raza" value={search} onChange={e => setSearch(e.target.value)}/>
+            <button onClick={Buscar}>Buscar</button>
+            {(razas != null) && 
+                razas.map(raza => {
+                    return(
+                        <div key={raza.mascotaid}>
+                            <p>ID {raza.mascotaid}</p>
+                            <p>Nombre {raza.nombre}</p>
+                            <p>Genero {raza.genero}</p>
+                            <p>Raza {raza.raza}</p>
+                            <p>Esepecie {raza.especie}</p>
+                            <p>Fecha de Nacimiento {raza.fechanacimiento}</p>
+                            <p>Cliente ID {raza.clienteid}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
     )
 }
 
--- src/pages/functionsPages/MaxMin.js
+++ src/pages/functionsPages/MaxMin.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const MaxMin = () => {
+
+    const [min, setMin] = useState()
+    const [max, setMax] = useState()
+
+    useEffect(() => {
+        fetch('http://localhost:8000/getData/Servicio/')
+          .then(response => response.json())
+          .then(data => {
+            let maxPrice = data[0]
+            let minPrice = data[0]
+      
+            data.map((item) => {
+                // console.log(`${item.precio} > ${maxPrice.precio}: ${item.precio > maxPrice.precio}`)
+                if(Number(item.precio) > Number(maxPrice.precio))
+                    maxPrice = item
+                if (Number(item.precio) < Number(minPrice.precio))
+                    minPrice = item;
+            })
+            
+            setMax(maxPrice);
+            setMin(minPrice);
+          });
+      }, []);
+      
+
     return(
-        <div>Maximos y minimos de Precios</div>
+        <div>
+            <h3>Presupuestos</h3>
+            <div>
+                <h5>Minimo Costo</h5>
+                <p>{min && min.nombre}</p>
+                <p>{min && min.descripcion}</p>
+                <p>{min && min.precio}</p>
+            </div>
+            <div>
+                <h5>Maximo Costo</h5>
+                <p>{max && max.nombre}</p>
+                <p>{max && max.descripcion}</p>
+                <p>{max && max.precio}</p>
+            </div>
+        </div>
     )
 }
 
